0ba6014d38fc0bf000bb056d77a7faa4aec478db
LN <489308542@qq.com>
2020-08-20 23:38:59 +0800

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,14 +13,12 @@ import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
     private  boolean flag;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final View inflate = LayoutInflater.from(this).inflate(R.layout.input_layout, null);
-        EditText editText = inflate.findViewById(R.id.ed1);
+        final EditText editText = inflate.findViewById(R.id.ed1);
         editText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -41,13 +39,16 @@ public class MainActivity extends AppCompatActivity {
                 View emjoLayout = inflate.findViewById(R.id.emjoLayout);
                 if (emjoLayout.getVisibility() == View.VISIBLE) {
                     emjoLayout.setVisibility(View.GONE);
-                    FrameLayout contentView = findViewById(R.id.contentView);
-                    contentView.removeView(inflate);
+                    editText.requestFocus();
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(editText,0);
+
                 } else {
                     emjoLayout.setVisibility(View.VISIBLE);
                     EditText viewById = inflate.findViewById(R.id.ed1);
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(viewById.getWindowToken(),0);
+
                 }
             }
         });
@@ -55,24 +56,23 @@ public class MainActivity extends AppCompatActivity {
         keyBordStateUtil.addOnKeyBordStateListener(new KeyBordStateUtil.onKeyBordStateListener() {
             @Override
             public void onSoftKeyBoardShow(int keyboardHeight) {
-                FrameLayout contentView = findViewById(R.id.contentView);
-                contentView.removeView(inflate);
-                contentView.addView(inflate);
-                inflate.setVisibility(View.VISIBLE);
+                    FrameLayout contentView = findViewById(R.id.contentView);
+                    contentView.removeView(inflate);
+                    contentView.addView(inflate);
+                    inflate.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onSoftKeyBoardHide() {
                 View emjoLayout = inflate.findViewById(R.id.emjoLayout);
-                EditText editText = inflate.findViewById(R.id.ed1);
                 if (emjoLayout.getVisibility() == View.VISIBLE) {
 
                 } else {
                     FrameLayout contentView = findViewById(R.id.contentView);
-                    editText.getInputType();
                     contentView.removeView(inflate);
                     inflate.setVisibility(View.GONE);
                     emjoLayout.setVisibility(View.GONE);
+
                 }
             }
         });
