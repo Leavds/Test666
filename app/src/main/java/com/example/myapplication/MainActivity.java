b95595ff84f7c2d007a61b72cb1e39f6fb0a9c25
LN <489308542@qq.com>
2020-08-20 22:07:31 +0800

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        KeyBordStateUtil keyBordStateUtil = new KeyBordStateUtil(this);
+        keyBordStateUtil.addOnKeyBordStateListener(new KeyBordStateUtil.onKeyBordStateListener() {
+            @Override
+            public void onSoftKeyBoardShow(int keyboardHeight) {
+                LinearLayout contentView = findViewById(R.id.contentView);
+                LinearLayout messageLayout = findViewById(R.id.messageLayout);
+                messageLayout.setClipChildren(false);
+                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) messageLayout.getLayoutParams();
+                layoutParams.width=contentView.getWidth();
+                messageLayout.setLayoutParams(layoutParams);
+                contentView.requestLayout();
+            }
+
+            @Override
+            public void onSoftKeyBoardHide() {
+                LinearLayout contentView = findViewById(R.id.contentView);
+                LinearLayout messageLayout = findViewById(R.id.messageLayout);
+                messageLayout.setClipChildren(true);
+                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) messageLayout.getLayoutParams();
+                layoutParams.width=0;
+                layoutParams.weight=1;
+                messageLayout.setLayoutParams(layoutParams);
+                contentView.requestLayout();
+            }
+        });
     }
 }
