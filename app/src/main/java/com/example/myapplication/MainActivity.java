e851a36c8c41821fa5e68497d17a675a7d98e3d7
LN <489308542@qq.com>
2020-08-20 23:23:18 +0800

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,28 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final View inflate = LayoutInflater.from(this).inflate(R.layout.input_layout, null);
+        EditText editText = inflate.findViewById(R.id.ed1);
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                    View emjoLayout = inflate.findViewById(R.id.emjoLayout);
+                    if(emjoLayout.getVisibility()==View.VISIBLE){
+                        flag=true;
+                    }else{
+                        flag=false;
+                    }
+                }
+                return flag;
+            }
+        });
         inflate.findViewById(R.id.iv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 View emjoLayout = inflate.findViewById(R.id.emjoLayout);
                 if (emjoLayout.getVisibility() == View.VISIBLE) {
                     emjoLayout.setVisibility(View.GONE);
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    EditText viewById = inflate.findViewById(R.id.ed1);
-                    imm.showSoftInput(viewById,0);
+                    FrameLayout contentView = findViewById(R.id.contentView);
+                    contentView.removeView(inflate);
                 } else {
                     emjoLayout.setVisibility(View.VISIBLE);
                     EditText viewById = inflate.findViewById(R.id.ed1);
