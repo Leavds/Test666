b66e62a0fd72003e246a650aa20568b2b8b17b88
LN <489308542@qq.com>
2020-08-20 23:04:33 +0800

    update

---
+++ app/src/main/java/com/example/myapplication/ChatActivity.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.widget.StateButton;
+
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class ChatActivity extends AppCompatActivity {
+
+    @BindView(R.id.llContent)
+    LinearLayout mLlContent;
+    @BindView(R.id.et_content)
+    EditText mEtContent;
+    @BindView(R.id.bottom_layout)
+    RelativeLayout mRlBottomLayout;//表情,添加底部布局
+    @BindView(R.id.ivAdd)
+    ImageView mIvAdd;
+    @BindView(R.id.ivEmo)
+    ImageView mIvEmo;
+    @BindView(R.id.btn_send)
+    StateButton mBtnSend;//发送按钮
+    @BindView(R.id.rlEmotion)
+    LinearLayout mLlEmotion;//表情布局
+
+     public static final String mSenderId="right";
+     public static final String mTargetId="left";
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        initContent();
+    }
+
+    protected void initContent() {
+        ButterKnife.bind(this) ;
+        initChatUi();
+     }
+
+
+    private void initChatUi(){
+        //mBtnAudio
+        final ChatUiHelper mUiHelper= ChatUiHelper.with(this);
+        mUiHelper.bindContentLayout(mLlContent)
+                .bindttToSendButton(mBtnSend)
+                .bindEditText(mEtContent)
+                .bindBottomLayout(mRlBottomLayout)
+                .bindEmojiLayout(mLlEmotion)
+                .bindToEmojiButton(mIvEmo)
+                .bindEmojiData();
+
+    }
+}
